ef959043281153a4f1c29b99e26ae6da80399d74
suss-gmez <60798050+sussgmez@users.noreply.github.com>
2021-12-03 08:40:49 -0400

    Delete login.php

--- ToDoList/views/login.php
+++
@@ -1,6 +0,0 @@
-<form id="form-login">
-	<h2>Iniciar Sesión</h2>
-	<input class="entry" type="text" name="user_login" placeholder="Nombre de Usuario" required>
-	<input class="entry" type="password" name="pass_login" placeholder="Contraseña" required>
-	<button>Ingresar</button>
-</form>

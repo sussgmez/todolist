85f2638a04a36842a5ca5e5bc42ccb96e941d91d
suss-gmez <60798050+sussgmez@users.noreply.github.com>
2021-12-03 08:41:08 -0400

    Add files via upload

---
+++ ToDoList/views/login/login.php
@@ -0,0 +1,6 @@
+<form id="form-login">
+	<h2>Iniciar Sesión</h2>
+	<input class="entry" type="text" name="user_login" placeholder="Nombre de Usuario" required>
+	<input class="entry" type="password" name="pass_login" placeholder="Contraseña" required>
+	<button>Ingresar</button>
+</form>
